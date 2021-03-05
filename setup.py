645f5f7a8a117f0eb97d6351dafefa41c3164c97
Craig Macdonald <craig.macdonald@glasgow.ac.uk>
2021-03-05 17:49:19 +0000

    specify packages explicitly

--- setup.py
+++ setup.py
@@ -4,20 +4,20 @@ with open('README.md') as f:
     readme = f.read()
 
 setup(
-   name='ANCE',
-   version='0.1.0',
-   description='Approximate Nearest Neighbor Negative Contrastive Learning for Dense Text Retrieval',
-   url='https://github.com/microsoft/ANCE',
-   classifiers=[
+    name='ANCE',
+    version='0.1.0',
+    description='Approximate Nearest Neighbor Negative Contrastive Learning for Dense Text Retrieval',
+    url='https://github.com/microsoft/ANCE',
+    classifiers=[
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.6',
         'Topic :: Scientific/Engineering :: Artificial Intelligence',
     ],
-   packages=find_packages(),
-   license="MIT",
-   long_description=readme,
-   install_requires=[
+    packages=['ANCE'] + ['ANCE.' + i for i in find_packages('ANCE')],
+    license="MIT",
+    long_description=readme,
+    install_requires=[
         'transformers==2.3.0', 
         'pytrec-eval',
         #'faiss-cpu',
